75dc9f8d0e22a68c5a0067623daa59a75a39604a
Arjo Magno <arjo@pop-os.localdomain>
2019-12-12 19:53:53 +0800

    asyncness

--- async-websocket-subscribe.py
+++ async-websocket-subscribe.py
@@ -11,66 +11,69 @@ import random
 import json
 import time
 import threading
+import asyncio
+from websockets import connect
 
-import websocket
 
-
-class GraphQLClient():
-    """
-    A simple GraphQL client that works over Websocket as the transport
-    protocol, instead of HTTP.
-    This follows the Apollo protocol.
-    https://github.com/apollographql/subscriptions-transport-ws/blob/master/PROTOCOL.md
-    """
-
-    def __init__(self, url):
-        self.ws_url = url
-        self._conn = websocket.create_connection(self.ws_url,
-                                                 on_message=self._on_message)
+class GraphQLClient:
+    async def __aenter__(self, url):
+        self.ws_url = url+'ws://hasura-midcodes1.herokuapp.com/v1/graphql'
+        self._conn = connect(self.ws_url, on_message=self._on_message)
+        self.websocket = await self._conn.__aenter__()   
         self._conn.on_message = self._on_message
-        self._subscription_running = False
-        self._st_id = None
+        self._subscription_running = False #OK
+        self._st_id = None #OK
+        return self
 
-    def _on_message(self, message):
-        data = json.loads(message)
+    async def __aexit__(self, *args, **kwargs):
+            await self._conn.__aexit__(*args, **kwargs)
+
+    async def _on_message(self, message):
+        data = await json.loads(message)
         # skip keepalive messages
         if data['type'] != 'ka':
             print(message)
 
-    def _conn_init(self, headers=None):
+    async def _conn_init(self, headers=None):
         payload = {
             'type': 'connection_init',
             'payload': {'headers': headers}
         }
-        self._conn.send(json.dumps(payload))
-        self._conn.recv()
+        await self.websocket.send(json.dumps(payload))
+        await self.websocket.recv()
 
-    def _start(self, payload):
-        _id = gen_id()
+    async def _start(self, payload):
+        _id = await gen_id()
         frame = {'id': _id, 'type': 'start', 'payload': payload}
-        self._conn.send(json.dumps(frame))
-        self._conn.recv()
+        await self.websocket.send(json.dumps(frame))
+        await self.websocket.recv()
         return _id
 
-    def _stop(self, _id):
+    async def _stop(self, _id):
         payload = {'id': _id, 'type': 'stop'}
         self._conn.send(json.dumps(payload))
-        return self._conn.recv()
+        return await self.websocket.recv()
 
-    def query(self, query, variables=None, headers=None):
+    async def query(self, query, variables=None, headers=None):
         self._conn_init(headers)
         payload = {'headers': headers, 'query': query, 'variables': variables}
         _id = self._start(payload)
-        self._conn.recv()
+        await self.websocket.recv()
         res = self._stop(_id)
         #print(dir(self._conn))
         return res
 
-    def subscribe(self, query, variables=None, headers=None, callback=None):
+
+    async def stop_subscribe(self, _id):
+        self._subscription_running = False
+        self._st_id.join()
+        self._stop(_id)
+
+    async def subscribe(self, query, variables=None, headers=None, callback=None):
         self._conn_init(headers)
         payload = {'headers': headers, 'query': query, 'variables': variables}
-        _cc = self._on_message if not callback else callback
-        _id = self._start(payload)
+        _cc = await self._on_message if not callback else callback
+        _id = await self._start(payload)
         def subs(_cc):
             self._subscription_running = True
             while self._subscription_running:
@@ -87,22 +90,16 @@ class GraphQLClient():
         self._st_id.start()
         return _id
 
-    def stop_subscribe(self, _id):
-        self._subscription_running = False
-        self._st_id.join()
-        self._stop(_id)
 
-    def close(self):
-        self._conn.close()
 
 
 # generate random alphanumeric id
-def gen_id(size=6, chars=string.ascii_letters + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+async def gen_id(size=6, chars=string.ascii_letters + string.digits):
+    return await ''.join(random.choice(chars) for _ in range(size))
 
 
 
-ws = GraphQLClient('ws://hasura-midcodes1.herokuapp.com/v1/graphql')
+ws = GraphQLClient()
 
 #add headers
 
@@ -112,7 +109,7 @@ def callback(_id, data):
 
 query = """
 subscription {
-  scanned(limit: 1, order_by: {created_at: desc}) {`
+  scanned(limit: 1, order_by: {created_at: desc}) {
     id
     scan
   }
@@ -127,59 +124,3 @@ time.sleep(subalive)
 print(f'{subalive} seconds over, stopping the subscription')
 ws.stop_subscribe(id)
 ws.close()
-
-
-
-
-#!/usr/bin/env python3
-
-import sys, json
-import asyncio
-from websockets import connect
-
-class EchoWebsocket:
-    async def __aenter__(self):
-        self._conn = connect('wss://ws.binaryws.com/websockets/v3')
-        self.websocket = await self._conn.__aenter__()        
-        return self
-
-    async def __aexit__(self, *args, **kwargs):
-        await self._conn.__aexit__(*args, **kwargs)
-
-    async def send(self, message):
-        await self.websocket.send(message)
-
-    async def receive(self):
-        return await self.websocket.recv()
-
-class mtest:
-    def __init__(self):
-        self.wws = EchoWebsocket()
-        self.loop = asyncio.get_event_loop()
-
-    def get_ticks(self):
-        return self.loop.run_until_complete(self.__async__get_ticks())
-
-    async def __async__get_ticks(self):
-        async with self.wws as echo:
-            await echo.send(json.dumps({'ticks_history': 'R_50', 'end': 'latest', 'count': 1}))
-            return await echo.receive()
-
-
-            from testws import *
-
-a = mtest()
-
-foo = a.get_ticks()
-print (foo)
-
-print ("async works like a charm!")
-
-foo = a.get_ticks()
-print (foo)
-
-
-root@ubupc1:/home/dinocob# python3 test.py
-{"count": 1, "end": "latest", "ticks_history": "R_50"}
-async works like a charm!
-{"count": 1, "end": "latest", "ticks_history": "R_50"}
